99c4f674cfd0ae12ab9ceceb95d6c0133bd16e45
Matt Vivier <mattvivier@gmail.com>
2026-07-23 16:01:53 -0400

    Initialize Pelican configuration in pelicanconf.py

---
+++ pelicanconf.py
@@ -0,0 +1,19 @@
+AUTHOR = 'Matt Vivier'
+SITENAME = 'Matt Vivier'
+SITEURL = 'https://mattvivier.com'
+
+PATH = 'content'
+OUTPUT_PATH = 'output'
+
+TIMEZONE = 'America/New_York'
+DEFAULT_LANG = 'en'
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = 10
+
+RELATIVE_URLS = False
